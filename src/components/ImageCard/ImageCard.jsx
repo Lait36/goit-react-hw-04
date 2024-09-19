45fdf35c39212297c0642a572c5eb7dbff0cb455
Lait36 <118002242+Lait36@users.noreply.github.com>
2024-09-19 18:28:45 +0300

    changing the ImageCard according to the task

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -4,9 +4,9 @@ export default function ImageCard({ item, openModal }) {
 
 
   return (
-    <div className={css.container} onClick={() => openModal(item)}>
+    <div className={css.container} >
      
-     <img className={css.img} src={item.urls.small} alt={item.alt_description } />
+     <img className={css.img} src={item.urls.small} alt={item.alt_description } onClick={() => openModal(item)} />
    
     </div>
   );
